4233dcf23f8a291e8f944b6bf2418530ff844c7c
Thanh Huy Duo <thanhhuy.duo@gmail.com>
2015-03-24 22:52:42 +0700

    added a new controller, add log4j configuration

---
+++ auke-service/src/main/java/com/auke/drone/ws/controllers/DroneController.java
@@ -0,0 +1,13 @@
+package com.auke.drone.ws.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by huyduong on 3/24/2015.
+ */
+@RequestMapping("/drone")
+@Controller
+public class DroneController {
+
+}
